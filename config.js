8fff498e872191624776238c1e7cad923aeaf72c
Earlpolloso <earlpolloso@gmail.com>
2024-06-30 11:50:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   token:
-    "",
-  channel: "1187655235229995058",
+    "MTI1NTA1Mzc0MzM2MDA1MzMzMA.G_CNNw.VQQGxMQb1kfpTXFlVEBhT1G1NF-KBg94WcEu_k",
+  channel: "1255143475158978580",
   data: "data.json",
   cf: {
     enable: true,
